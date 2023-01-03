17639a7498c7e105b16f3fb6827d670adb8b0aa7
Johan Jarvi <johan.jarvi@gmail.com>
2023-01-03 14:47:41 +1000

    Add time left functionality

--- src/components/TimerDisplay/TimerDisplay.tsx
+++ src/components/TimerDisplay/TimerDisplay.tsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 import { convertSecondsToHoursMinutesSecondsString } from "../../utils/TimeConverter";
 import "./TimerDisplay.css";
 
-interface TimeWorkedDisplayProps {
-  timeWorkedSeconds: number;
+interface TimerDisplayProps {
+  seconds: number;
 }
 
-export const TimeWorkedDisplay = (props: TimeWorkedDisplayProps) => {
+export const TimerDisplay = (props: TimerDisplayProps) => {
   const [timeWorkedDisplay, setTimeWorkedDisplay] = useState("00:00:00");
 
   useEffect(() => {
     setTimeWorkedDisplay(
-      convertSecondsToHoursMinutesSecondsString(props.timeWorkedSeconds)
+      convertSecondsToHoursMinutesSecondsString(props.seconds)
     );
   }, [props]);
 
