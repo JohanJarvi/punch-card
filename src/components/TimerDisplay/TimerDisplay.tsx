d9553de36c4732aa79e0823527f0032d591d6355
Johan Jarvi <johan.jarvi@gmail.com>
2023-04-13 15:48:00 +1000

    Add X day total

--- src/components/TimerDisplay/TimerDisplay.tsx
+++ src/components/TimerDisplay/TimerDisplay.tsx
@@ -9,33 +9,30 @@ interface TimerDisplayProps {
 
 export const TimerDisplay = (props: TimerDisplayProps) => {
   const [timeWorkedDisplay, setTimeWorkedDisplay] = useState("00:00:00");
-  const [ displayMessage, setDisplayMessage ] = useState(props.message);
+  const [displayMessage, setDisplayMessage] = useState(props.message);
 
   useEffect(() => {
-    if(props.seconds < 0) {
-      setDisplayMessage("Overtime worked: ")
-      setTimeWorkedDisplay(convertSecondsToHoursMinutesSecondsString(Math.abs(props.seconds)));
+    if (props.seconds < 0) {
+      setDisplayMessage("Overtime worked: ");
+      setTimeWorkedDisplay(
+        convertSecondsToHoursMinutesSecondsString(Math.abs(props.seconds))
+      );
     } else {
-
-
-    setTimeWorkedDisplay(
-      convertSecondsToHoursMinutesSecondsString(props.seconds)
-    );
+      setTimeWorkedDisplay(
+        convertSecondsToHoursMinutesSecondsString(props.seconds)
+      );
     }
-
   }, [props]);
 
   return (
     <div>
-        <p>
-          <strong>{displayMessage}</strong>{" "}
-          <span className="clock">{timeWorkedDisplay}</span>
-        </p>
+      <p>
+        <strong>{displayMessage}</strong>{" "}
+        <span className="clock">{timeWorkedDisplay}</span>
+      </p>
       {props.seconds < 0 ? (
         <p>You are now working overtime. Stop that!</p>
-      ) : (
-        null
-      )}
+      ) : null}
     </div>
   );
 };
