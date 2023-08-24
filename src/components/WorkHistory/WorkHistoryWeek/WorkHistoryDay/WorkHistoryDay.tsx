da7446b9234cfb7669001c3cf9d960e711d51343
Johan Jarvi <johan.jarvi@gmail.com>
2023-08-24 14:36:23 +1000

    Make todays date bold in history

--- src/components/WorkHistory/WorkHistoryWeek/WorkHistoryDay/WorkHistoryDay.tsx
+++ src/components/WorkHistory/WorkHistoryWeek/WorkHistoryDay/WorkHistoryDay.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Workday } from "../../../../types/WorkHistory";
 import { convertSecondsToHoursMinutesSecondsString } from "../../../../utils/TimeConverter";
 
@@ -6,10 +7,17 @@ interface WorkHistoryDayProps {
 }
 
 export const WorkHistoryDay = ({ day }: WorkHistoryDayProps) => {
+  const isToday = useMemo(
+    () => new Date().toLocaleDateString() === day.date,
+    [day]
+  );
+
   return (
-    <div className="flex flex-row justify-around">
-      <strong>{day.date}</strong>
-      <em>{convertSecondsToHoursMinutesSecondsString(day.time)}</em>
+    <div className="flex flex-row justify-around font-mono">
+      <span className={`${isToday && "font-bold"}`}>{day.date}</span>
+      <span className={`${isToday && "font-bold"}`}>
+        {convertSecondsToHoursMinutesSecondsString(day.time)}
+      </span>
     </div>
   );
 };
